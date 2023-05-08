df082c4876270a4ba8608b71bc5d1fd0aa003b81
Huzefa Bin Juned <huzefaghurna@gmail.com>
2023-05-08 15:53:39 +0530

    UserForm created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import UserForm from "./components/UserForm";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <UserForm />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </>
   );
 }
 
---
+++ src/components/UserForm.jsx
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import "../components/UserForm.css";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import { toast } from "react-toastify";
+
+const UserForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [gender, setGender] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+  const resetForm = () => {
+    console.log(name, email, password, cPassword, gender, mobile);
+    setName("");
+    setEmail("");
+    setMobile("");
+    setGender("");
+    setPassword("");
+    setCPassword("");
+    console.log(name, email, password, cPassword, gender, mobile);
+  };
+
+  const onHandleSubmit = () => {
+    const regexForName = /^[A-Za-z ]{3,15}$/;
+                        
+    const regexForEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    const regexForPassword =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/;
+    const data = {
+      name,
+      email,
+      mobile,
+      gender,
+      password,
+    };
+    if (!name || !email || !mobile || !gender || !password) {
+      toast.error("All Fields are Required");
+    } else if (!regexForName.test(name)) {
+      toast.error(
+        "Name should  not Contains any Number or Special  Chars,  Min length 3 and Max 15 Acceptable "
+      );
+    } else if (!regexForEmail.test(email)) {
+      toast.error(" Invalid Email Input");
+    } else if (!regexForPassword.test(password)) {
+      toast.error(
+        " Password must Contains  Uppercase, Lowercase, Digit,Special character, and Min eight"
+      );
+    } else if (password !== cPassword) {
+      toast.error("Password Doesn't Matched");
+    } else {
+      toast.success("validate and saved");
+      resetForm();
+    }
+  };
+
+  return (
+    <div className="main">
+      <h2 id="heading">ReactJs Form</h2>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="email"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <div className="contact">
+          <PhoneInput
+            placeholder="Enter phone number"
+            value={mobile}
+            onChange={setMobile}
+          />
+        </div>
+
+        <div className="selectGender">
+          <input
+            id="radioBtn"
+            type="radio"
+            value="Male"
+            name="gender"
+            placeholder="Male"
+            onChange={(e) => setGender(e.target.value)}
+          />{" "}
+          Male
+          <input
+            id="radioBtn"
+            type="radio"
+            value="Female"
+            name="gender"
+            onChange={(e) => setGender(e.target.value)}
+          />{" "}
+          Female
+        </div>
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={cPassword}
+          onChange={(e) => setCPassword(e.target.value)}
+        />
+        <button id="submitBtn" onClick={() => onHandleSubmit()}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default UserForm;
